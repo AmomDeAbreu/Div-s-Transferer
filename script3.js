2124369af7a4d5fc5c50723f77510355e477b086
Amom Ao Cubo <102225054+AmomDeAbreu@users.noreply.github.com>
2024-02-08 15:38:47 -0300

    commented the code

--- script3.js
+++ script3.js
@@ -1,28 +1,3 @@
-/*const amom = document.querySelector("#a1");
-const alehandro = document.querySelector("div.nome.a2")
-const names = [...document.querySelectorAll("div.nome")];
-const message = () => {
-    alert("Hello world crap");
-}
-
-//amom.addEventListener("click", message); // usando função normal
-//alehandro.addEventListener("click", () => {alert("Clicou também")}); // usando arrow function
-
-
-names.map((el) => { // se quiser tratar todas as divs, é necessário dar um spread, percorrer e em seguida adicionar event listener
-    el.addEventListener("click", () => {
-        el.classList.add("priority");
-    })
-})
-
-
-const albuquerque = document.querySelector("#albuq");
-albuquerque.addEventListener("click", (evt) => {  // evt é um parâmetro que recebe o evento que acabou de ser executado.
-    const element = evt.target; // dentro do evento sendo recebido existem várias características e dentre eles, o .target é o objeto que está envolvido no evento. (A própria div)
-    element.innerHTML = "Trollei";
-})*/
-
-
 const botao = document.querySelector("#botao");
 const copia = document.querySelector("#copia");
 const nomes = [...document.querySelectorAll(".nome")];
@@ -50,6 +25,7 @@ const transfer = () => {
                 e.classList.remove("selecionado");
                 e.classList.remove("c");
                 e.classList.add("m");
+
             }
     })
 }
